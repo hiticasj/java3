df830b04b84d63fd6b4846391449afcd0387b94a
hiticasj <07-chirp-hone@icloud.com>
2024-04-25 22:32:21 -0400

    Added first path with ctx.beginPath();, strokeStyle specifies color, gradient, or pattern to use for strokes(outlines) around shapes. Arranged where line will be on canvas with moveTo(); and lineTo();, and closed path with .stroke();.

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const canvas = document.getElementById("my-house");
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 
-// Set line width
-ctx.lineWidth = 10;
-
-// Wall
-ctx.strokeRect(75, 140, 150, 110);
-
-// Door
-ctx.fillRect(130, 190, 40, 60);
-
-// Roof
+// First path
 ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
+ctx.strokeStyle = "blue";
+ctx.moveTo(20, 20);
+ctx.lineTo(200, 20);
+ctx.stroke();
+
+// Second path
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.moveTo(20, 20);
+ctx.lineTo(120, 120);
 ctx.stroke();
