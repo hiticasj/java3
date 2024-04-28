463a26b53868f7bce57f99ae8313a49f5a519037
hiticasj <07-chirp-hone@icloud.com>
2024-04-28 03:12:10 -0400

    Added addEventListener to make clicking on myP/myDiv element turn it lightblue.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+/* Event Listeners */
+
+//Syntax: element.addEventListener(event, function, useCapture);
+
 var myP = document.getElementById("myP");
 var myDiv = document.getElementById("myDiv");
 var myP2 = document.getElementById("myP2");
 var myDiv2 = document.getElementById("myDiv2");
+
+myP.addEventListener("click", function() { myDiv.style.background = "lightblue"});
