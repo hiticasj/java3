8288d759d48e72da0166daa527df9106392406e6
hiticasj <07-chirp-hone@icloud.com>
2024-04-28 03:30:19 -0400

    Added addEventListener with function changeText for myP2.

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ var myDiv = document.getElementById("myDiv");
 var myP2 = document.getElementById("myP2");
 var myDiv2 = document.getElementById("myDiv2");
 
-myP.addEventListener("click", function() { myDiv.style.background = "lightblue"});
+myP.addEventListener("click", function() { myDiv.style.background = "lightblue" });
+
+// myP.onclick = function() {"click", function() { myDiv.style.background = "lightblue"}};
+
+myP.addEventListener("click", changeText);
+
+function changeText() {
+    myP2.textContent = "According to a Cornell publication, the answer is ~700 pounds."
+};
