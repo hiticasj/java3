733fa7f20c621bbe26e02f6d7596966d76236838
hiticasj <07-chirp-hone@icloud.com>
2024-05-01 22:27:47 -0400

    Added var form to link myForm with getElementById. Added event listener for input. event.target property gives reference to the input field that triggered the event,allowing us to check validty with checkValidity method. if form is invalid, an alert is shown indicating which field is invalid.

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ buttonContainer.addEventListener("click", function(event) {
     alert(event.target.innerText + " was clicked!");
     }
 }, true); 
+
+var form = document.getElementById("myForm");
+
+var form = document.getElementById('myForm');
+
+form.addEventListener('input', function(event) {
+    var inputField = event.target;
+    var isValid = inputField.checkValidity();
+    
+    if (!isValid) {
+        alert(inputField.name + " is invalid!");
+    }
+});
