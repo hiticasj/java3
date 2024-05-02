880c1f049430189a97eda7af4267ba5a112b4a5b
hiticasj <07-chirp-hone@icloud.com>
2024-05-01 22:08:01 -0400

    Also setting useCapture to true allows us to set one event listener to all divs with action-button, instead of using a new event listener for each button in the div.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ buttonContainer.addEventListener("click", function(event) {
     if (event.target.classList.contains("action-button")) {
     alert(event.target.innerText + " was clicked!");
     }
-}, true);
+}, true); 
